7bd385402ab0738bfb1730a9b850cd1b55db2a79
senthilbalajiganesan <g.senthilbalaji@gmail.com>
2018-07-29 20:22:33 +0530

    Added header and necesssary files

---
+++ src/components/common/NavBar.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+import "../../styles/Header.css";
+
+class NavBar extends Component {
+  render() {
+    return (
+        <div className="background-header">
+          <div className="font-header">
+            Web Portfolio
+          </div>
+        </div>
+    )
+  }
+}
+
+export default NavBar;
